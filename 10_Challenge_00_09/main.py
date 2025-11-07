b4e4f183f6e8293cc355ea808d0af52b1e1b6e81
LouisZCode <lgzg90@hotmail.com>
2025-11-07 12:57:02 +0100

    feature: made the agent answers pretty in Terminal, start of the csv logic

--- 10_Challenge_00_09/main.py
+++ 10_Challenge_00_09/main.py
@@ -1,10 +1,23 @@
+from turtle import pd
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.agents.middleware import HumanInTheLoopMiddleware
 #from langchain.agents.middleware #how to get the dynamic prompting part!?
 from langchain_core.messages import HumanMessage, AIMessage
 
+import os
+
+import pandas as pd
+import gradio as gr
+
 from langchain.tools import tool
+
+APPLICATIONS = "applications.csv"
+
+if os.path.exists(APPLICATIONS):
+    new_datafreame = pd.DataFrame(APPLICATIONS, columns=["company", "position", "status", "deadline", "applied_date", "salary_range", "notes"])
+    new_datafreame.to_csv(APPLICATIONS, index=False)
+    #Save the database?
 
 load_dotenv()
 
@@ -18,4 +31,6 @@ result = agent.invoke({
     "messages": message
 })
 
-print(result)
+#Make the answers pretty for now
+for i,msg in enumerate(result["messages"]):
+    msg.pretty_print()
