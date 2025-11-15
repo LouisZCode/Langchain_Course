08052b7a041828eca4dd2c7c7459abf33d757718
LouisZCode <lgzg90@hotmail.com>
2025-11-15 15:57:20 +0100

    feature: new AML to save all system prompt

--- 12_Challenge_Finantial_Agent/main.py
+++ 12_Challenge_Finantial_Agent/main.py
@@ -1,9 +1,30 @@
 from ollama_agent import call_ollama
+import gradio as gr
 
 
+import yaml
 
 
-##Test of Ollama Agent:
-"""query = input("Your message:\n")
-answer = call_ollama(query)
-print(answer)"""
+with open("prompts.yaml", "r", encoding="utf-8") as f:
+    prompts = yaml.safe_load(f)
+
+    prompt = prompts["HISTORIAN"]
+
+
+def response (message, history):
+
+    messages = history + [{"role": "user", "content": message}]
+
+    response = call_ollama(prompt, messages)
+
+    return response
+
+with gr.Blocks() as demo:
+    with gr.Tabs():
+
+        with gr.Tab("Tab 1"):
+            gr.ChatInterface(
+                fn=response
+            )
+
+demo.launch()
--- 12_Challenge_Finantial_Agent/ollama_agent.py
+++ 12_Challenge_Finantial_Agent/ollama_agent.py
@@ -2,11 +2,9 @@ from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 
 
-def call_ollama(query : str) -> str:
-    SYSTEM_PROMPT = """
-    You are a full stack comedian. There is no audience. Keep the Joke short.
-    You do not explain the joke
-    """
+
+def call_ollama(system_prompt : str, query : str) -> str:
+
 
     model = ChatOllama(
         model="gemma3:27b"
@@ -14,15 +12,12 @@ def call_ollama(query : str) -> str:
 
     agent = create_agent(
         model=model,
-        system_prompt=SYSTEM_PROMPT
+        system_prompt=system_prompt
     )
 
-    message = query
-
     result = agent.invoke({
-        "role" : "user",
-        "messages" : message
-    }
+        "messages" : query
+        }
     )
 
     return result["messages"][1].content
