6716480052e631bbbbd45a3255a24691619418ce
LouisZCode <lgzg90@hotmail.com>
2025-11-15 14:49:04 +0100

    feature: add of local Ollama LLM for first test

--- 12_Challenge_Finantial_Agent/main.py
+++ 12_Challenge_Finantial_Agent/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from 12-challenge-finantial-agent!")
+from errno import ENETUNREACH
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
 
+SYSTEM_PROMPT = """
+You are a full stack comedian. There is no audience. Keep the Joke short.
+You do not explain the joke
+"""
 
-if __name__ == "__main__":
-    main()
+model = ChatOllama(
+    model="gemma3:27b"
+)
+
+agent = create_agent(
+    model=model,
+    system_prompt=SYSTEM_PROMPT
+)
+
+message = "Tell me a joke about France"
+
+result = agent.invoke({
+    "role" : "user",
+    "messages" : message
+}
+)
+
+for i,msg in enumerate(result["messages"]):
+    msg.pretty_print()
