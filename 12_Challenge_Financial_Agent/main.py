b3a7cf0c7580720a540a4d9714438d7631f23a61
LouisZCode <lgzg90@hotmail.com>
2025-11-17 13:30:48 +0100

    feature: add of memory to the agent

--- 12_Challenge_Financial_Agent/main.py
+++ 12_Challenge_Financial_Agent/main.py
@@ -1,15 +1,18 @@
 from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import InMemorySaver
 
 import gradio as gr
 import yaml
 
 
+THREAD_ID = "thread_001"
+
 with open("prompts.yaml", "r", encoding="utf-8") as f:
     prompts = yaml.safe_load(f)
     prompt = prompts["HISTORIAN"]
 
-print(prompt)
+#print(prompt)
 
 model = ChatOllama(
     model="gemma3:27b"
@@ -17,18 +20,18 @@ model = ChatOllama(
 
 agent = create_agent(
     model=model,
-    system_prompt=prompt
+    system_prompt=prompt,
+    checkpointer=InMemorySaver()
 )
 
-
-# TODO - Give it Memory so it remembers the ocnversation
 
 
 ##Gradio-ing
 def response(message, history):
 
     response = agent.invoke(
-        {"messages": [{"role": "user", "content": message}]}
+        {"messages": [{"role": "user", "content": message}]},
+        {"configurable": {"thread_id": THREAD_ID}}
     )
     return response["messages"][-1].content
 
